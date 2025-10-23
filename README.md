9758300ba81d5030ea26600ccc6f267a5adcf0db
Raisat <raisat.bekbulatova1234@gmail.ru>
2025-10-23 03:05:44 +0300

    fixed bugs

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # java-filmorate
 Template repository for Filmorate project.
+
+11 sprint
+- add friends
+- add likes
--- src/main/java/ru/yandex/practicum/filmorate/messages/ValidationExceptionMessages.java
+++ src/main/java/ru/yandex/practicum/filmorate/messages/ValidationExceptionMessages.java
@@ -1,9 +1,9 @@
 package ru.yandex.practicum.filmorate.messages;
 
 public enum ValidationExceptionMessages {
-    FRIEND_TO_FRIEND("Пользователь не может быть другом сам себе"),
-    NOT_FRIENDS("Пользователи не друзья"),
-    WITHOUT_LIKE("Пользователь не ставил лайк фильму");
+    FRIEND_TO_FRIEND("Пользователь не может быть другом сам себе");
+   // NOT_FRIENDS("Пользователи не друзья"),
+   // WITHOUT_LIKE("Пользователь не ставил лайк фильму");
 
 
     private final String text;
