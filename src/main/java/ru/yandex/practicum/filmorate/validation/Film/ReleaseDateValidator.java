2da6d3802d0ca1e008729ece6acbd845e8b41d4d
Raisat <raisat.bekbulatova1234@gmail.ru>
2025-10-23 11:30:31 +0300

    Revert "изменения для проверки валидации"
    This reverts commit 56ea4d08c0e33a0a7a13d8b3368340ef39da9d08.

--- src/main/java/ru/yandex/practicum/filmorate/validation/Film/ReleaseDateValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validation/Film/ReleaseDateValidator.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.validation.Film;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import ru.yandex.practicum.filmorate.model.Film;
 
 import java.time.LocalDate;
 
